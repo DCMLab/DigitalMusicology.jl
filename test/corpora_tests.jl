928767742507ae9574dd837e3b6395b989d3d556
Christoph Finkensiep <chfin@chfin.de>
2018-03-07 17:32:14 +0100

    add actual file reading tests to corpora tests

--- test/corpora_tests.jl
+++ test/corpora_tests.jl
@@ -21,6 +21,15 @@ struct MyTestCorpus <: Corpus end
         @test isempty(dirs("0/"))
         @test isempty(pieces("./"))
         @test pieces("0/") == Set(["0/ursatz"])
-        @test ls() == ["0/"]        
+        @test ls() == ["0/"]
+
+        ns = getpiece("0/ursatz", :notes_secs)
+        @test pitches(ns) == @midi [76, 48, 74, 55, 72, 48]
+        @test map(onset, ns) == [0.0, 0.0, 1.0, 1.0, 2.0, 2.0]
+        @test map(offset, ns) == [911/960, 911/960, 1871/960, 1871/960, 3743/960, 3743/960]
+
+        nw = getpiece("0/ursatz", :notes_wholes)
+        @test map(onset, nw) == [0//1, 0//1, 1//2, 1//2, 1//1, 1//1]
+        @test map(offset, nw) == [911//1920, 911//1920, 1871//1920, 1871//1920, 3743//1920, 3743//1920]
     end
 end
