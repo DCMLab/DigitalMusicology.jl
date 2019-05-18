326cf93ad67df9e03bdbdf032246a04b833886aa
Christoph Finkensiep <chfin@chfin.de>
2019-05-18 13:44:14 +0200

    parse ties in MusicXML

--- src/io/musicxmlfiles.jl
+++ src/io/musicxmlfiles.jl
@@ -5,7 +5,25 @@ using DataFrames
 
 export musicxmlnotes
 
-testfile = "/home/chfin/Uni/phd/data/csapp/mozart-piano-sonatas/musicxml/sonata15-1.xml"
+testfile = "/home/chfin/Uni/phd/data/csapp/mozart-piano-sonatas/musicxml/sonata03-3.xml"
+
+# TODOs:
+# - timewise parsing
+# - modelling repetitions:
+#   - unfold and use duplicate IDs
+#   - allows grouping by id: each note has 1 on/off per repetition
+
+const NoteTuple = Tuple{Rational{Int},Rational{Int},Int,Int,Union{String,Missing}}
+
+mutable struct PartState
+    div :: Int
+    time :: Rational{Int}
+    prev_time :: Rational{Int}
+    trans_dia :: Int
+    trans_chrom :: Int
+    tied :: Vector{NoteTuple}
+end
+PartState() = PartState(1, 0//1, 0//1, 0, 0, [])
 
 function musicxmlnotes(file)
     doc = parse_file(file)
@@ -37,15 +55,6 @@ function partwise(score)
     end
     sort!(notes, [:onset])
 end
-
-mutable struct PartState
-    div :: Int
-    time :: Rational{Int}
-    prev_time :: Rational{Int}
-    trans_dia :: Int
-    trans_chrom :: Int
-end
-PartState() = PartState(1, 0//1, 0//1, 0, 0)
 
 function partnotes(part)
     state = PartState()
@@ -99,13 +108,12 @@ function attribs!(attr, state)
             state.trans_chrom = firstint(node, "chromatic")
             state.trans_dia = firstintor(node, "diatonic", 0)
             octs = firstintor(node, "octave-change", 0)
-            state.trans_dia = state.trans_dia + 7 * octs
-            state.trans_chrom = state.trans_chrom + 12 * octs 
+            state.trans_dia += 7 * octs
+            state.trans_chrom += 12 * octs 
         end
     end
 end
 
-# TODO: ties (needs state: keep tie-starting note, push when completed)
 function note!(note, notes, state)
     # time
     duration = firstint(note, "duration", 0) // (state.div * 4) # might be grace note
@@ -114,7 +122,7 @@ function note!(note, notes, state)
     else
         onset = state.time
         state.prev_time = state.time
-        state.time = state.time + duration
+        state.time += duration
     end
     offset = onset + duration
 
@@ -126,26 +134,51 @@ function note!(note, notes, state)
         alt = firstint(epitch, "alter", 0)
         
         dia, chrom = notenames[firstcont(epitch, "step")]
-        dia = dia + state.trans_dia + oct * 7
-        chrom = chrom + alt + state.trans_chrom + oct * 12
+        dia += state.trans_dia + oct * 7
+        chrom += alt + state.trans_chrom + oct * 12
         
         # id
         attr = attributes_dict(note)
         id = get(attr, "id", get(attr, "xml:id", missing))
         
-        push!(notes, (onset, offset, dia, chrom, id))
+        # tie stop?
+        if any(t -> attribute(t, "type") == "stop", note["tie"])
+            
+            # find corresponding open note
+            istart = findfirst(state.tied) do (ton, toff, tdia, tchrom, tid)
+                toff == onset && tdia == dia && tchrom == chrom
+            end
+
+            # combine info
+            if istart != nothing
+                (ton, toff, tdia, tchrom, tid) = state.tied[istart]
+                onset = ton
+                if ismissing(tid)
+                    id = tid
+                end
+                deleteat!(state.tied, istart) # delete open note
+            end
+        end
+
+        # tie start?
+        if any(t -> attribute(t, "type") == "start", note["tie"])
+            # note starts tie: add to open notes
+            push!(state.tied, (onset, offset, dia, chrom, id))
+        else # note is complete: add to output
+            push!(notes, (onset, offset, dia, chrom, id))
+        end
     end
 end
 
 function forward!(node, state)
     duration = firstint(node, "duration", 0) // (state.div * 4)
-    state.time = state.time + duration
+    state.time += duration
     state.prev_time = state.time # reset
 end
 
 function backup!(node, state)
     duration = firstint(node, "duration", 0) // (state.div * 4)
-    state.time = state.time - duration
+    state.time -= duration
     state.prev_time = state.time # reset
 end
 
